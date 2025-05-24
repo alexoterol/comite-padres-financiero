94105fbd4cefce8b8971870cad2210f28d5a658e
alexoterol <adotero@espol.edu.ec>
2025-05-24 16:04:57 -0500

    Avance en mostrar los datos de la cuenta de usuario y su perfil. Se han añadido estilos CSS para mejorar la presentación de la información. También se han realizado cambios en el archivo index.html para incluir los nuevos elementos. Pruebas con SheetJS para la gestión de datos de usuario y su perfil. Investigación sobre la gestión de datos de usuario y su perfil. Realizar estilos en el footer y evitar redundancia en el CSS.

---
+++ JS/code.js
@@ -0,0 +1,26 @@
+
+
+// fetch('https://script.google.com/macros/s/AKfycbzlV_QRLa_GvfehGiFBj8-J59fjr-UlPj9vo3BPxFazYFwyR4mxJOHwm8SmsazvZlAxXg/exec')
+// .then(res => res.json())
+// .then(data => {
+//     const container = document.getElementById('info');
+//     data.forEach(row => {
+//     const card = document.createElement('div');
+//     card.innerHTML = `<h3>${row.Nombre}</h3>
+//                         <p>Ahorro: ${row.Ahorro} $</p>
+//                         <p>Deuda: ${row.Deuda} $</p>`;
+//     container.appendChild(card);
+//     });
+// })
+// .catch(err => console.error('Error al cargar datos:', err));
+
+// fetch('https://script.google.com/macros/s/AKfycbzlV_QRLa_GvfehGiFBj8-J59fjr-UlPj9vo3BPxFazYFwyR4mxJOHwm8SmsazvZlAxXg/exec')
+// .then(res => res.json())
+// .then(data => {
+// document.getElementById('info').innerHTML = `
+//     <h2>${data.Nombre}</h2>
+//     <p>Ahorro: ${data.Ahorro} $</p>
+//     <p>Deuda: ${data.Deuda} $</p>
+// `;
+// })
+// .catch(err => console.error('Error al cargar datos 2:', err));
---
+++ JS/toggleDetails.js
@@ -0,0 +1,4 @@
+function toggleDetalle(id) {
+  const el = document.getElementById(id);
+  el.classList.toggle('hidden');
+}
