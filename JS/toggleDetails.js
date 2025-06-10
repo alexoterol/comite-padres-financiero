5cee3bc3d7d5b40ecf45bc7a10d33366dd1882fc
alexoterol <adotero@espol.edu.ec>
2025-06-10 12:57:24 -0500

    Corrección de estilo y normalización de contenido.

--- JS/toggleDetails.js
+++ JS/toggleDetails.js
@@ -2,3 +2,14 @@ function toggleDetalle(id) {
   const el = document.getElementById(id);
   el.classList.toggle('hidden');
 }
+
+// Cierra el modal si haces clic fuera del contenido
+document.addEventListener('click', function(event) {
+  const modales = document.querySelectorAll('.modal');
+
+  modales.forEach(modal => {
+    if (!modal.classList.contains('hidden') && event.target === modal) {
+      modal.classList.add('hidden');
+    }
+  });
+});
